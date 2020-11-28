c2be35d3ea941abd310589325fe5313709067992
AlexK <alex68604477@gmail.com>
2020-11-28 21:47:01 +0200

    HomeWork3

---
+++ app/src/main/java/com/ook/academy/project/FragmentMoviesDetails.kt
@@ -0,0 +1,24 @@
+package com.ook.academy.project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesDetails : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_movies_details, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.path).setOnClickListener {
+            activity?.onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/com/ook/academy/project/FragmentMoviesList.kt
@@ -0,0 +1,28 @@
+package com.ook.academy.project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.movie).setOnClickListener {
+            activity?.apply {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, FragmentMoviesDetails()).addToBackStack(null)
+                    .commit()
+            }
+        }
+    }
+
+}
